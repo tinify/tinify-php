e53e5a6837cdb0f3c85a1a5332aff3d83fd5a2da
Rolf Timmermans <rolftimmermans@voormedia.com>
2016-04-01 10:35:40 +0200

    Add integration test for metadata preservation.

--- test/integration.php
+++ test/integration.php
@@ -17,22 +17,61 @@ class ClientIntegrationTest extends PHPUnit_Framework_TestCase {
     public function testShouldCompressFromFile() {
         $path = tempnam(sys_get_temp_dir(), "tinify-php");
         self::$optimized->toFile($path);
-        $this->assertGreaterThan(0, filesize($path));
-        $this->assertLessThan(1500, filesize($path));
+
+        $size = filesize($path);
+        $contents = fread(fopen($path, "rb"), $size);
+
+        $this->assertGreaterThan(1000, $size);
+        $this->assertLessThan(1500, $size);
+
+        /* width == 137 */
+        $this->assertContains("\0\0\0\x89", $contents);
+        $this->assertNotContains("Copyright Voormedia", $contents);
+    }
+
+    public function testShouldCompressFromUrl() {
+        $path = tempnam(sys_get_temp_dir(), "tinify-php");
+        $source = \Tinify\fromUrl("https://raw.githubusercontent.com/tinify/tinify-php/master/test/examples/voormedia.png");
+        $source->toFile($path);
+
+        $size = filesize($path);
+        $contents = fread(fopen($path, "rb"), $size);
+
+        $this->assertGreaterThan(1000, $size);
+        $this->assertLessThan(1500, $size);
+
+        /* width == 137 */
+        $this->assertContains("\0\0\0\x89", $contents);
+        $this->assertNotContains("Copyright Voormedia", $contents);
     }
 
     public function testShouldResize() {
         $path = tempnam(sys_get_temp_dir(), "tinify-php");
         self::$optimized->resize(array("method" => "fit", "width" => 50, "height" => 20))->toFile($path);
-        $this->assertGreaterThan(0, filesize($path));
-        $this->assertLessThan(1000, filesize($path));
+
+        $size = filesize($path);
+        $contents = fread(fopen($path, "rb"), $size);
+
+        $this->assertGreaterThan(500, $size);
+        $this->assertLessThan(1000, $size);
+
+        /* width == 50 */
+        $this->assertContains("\0\0\0\x32", $contents);
+        $this->assertNotContains("Copyright Voormedia", $contents);
     }
 
-    public function testShouldCompressFromUrl() {
+    public function testShouldPreserveMetadata() {
         $path = tempnam(sys_get_temp_dir(), "tinify-php");
-        $optimized = \Tinify\fromUrl("https://raw.githubusercontent.com/tinify/tinify-php/master/test/examples/voormedia.png");
-        $optimized->toFile($path);
-        $this->assertGreaterThan(0, filesize($path));
-        $this->assertLessThan(1500, filesize($path));
+        self::$optimized->preserve("copyright", "creation")->toFile($path);
+
+        $size = filesize($path);
+        $contents = fread(fopen($path, "rb"), $size);
+
+        $this->assertGreaterThan(1000, $size);
+        $this->assertLessThan(2000, $size);
+
+        /* width == 137 */
+        $this->assertContains("\0\0\0\x89", $contents);
+        $this->assertContains("Copyright Voormedia", $contents);
     }
 }
