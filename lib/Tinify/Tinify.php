9a4274a490bec626b80c2c2083f1f4349b971e81
Bruce Wells <brucekwells@gmail.com>
2023-03-02 10:05:37 -0500

    Move Tinify\Tinify for proper autoloading and break out functions.php

--- lib/Tinify/Tinify.php
+++ lib/Tinify/Tinify.php
@@ -2,9 +2,8 @@
 
 namespace Tinify;
 
-const VERSION = "1.6.1";
-
 class Tinify {
+
     private static $key = NULL;
     private static $appIdentifier = NULL;
     private static $proxy = NULL;
@@ -49,48 +48,5 @@ class Tinify {
 
     public static function setClient($client) {
         self::$client = $client;
-    }
-}
-
-function setKey($key) {
-    return Tinify::setKey($key);
-}
-
-function setAppIdentifier($appIdentifier) {
-    return Tinify::setAppIdentifier($appIdentifier);
-}
-
-function setProxy($proxy) {
-    return Tinify::setProxy($proxy);
-}
-
-function getCompressionCount() {
-    return Tinify::getCompressionCount();
-}
-
-function compressionCount() {
-    return Tinify::getCompressionCount();
-}
-
-function fromFile($path) {
-    return Source::fromFile($path);
-}
-
-function fromBuffer($string) {
-    return Source::fromBuffer($string);
-}
-
-function fromUrl($string) {
-    return Source::fromUrl($string);
-}
-
-function validate() {
-    try {
-        Tinify::getClient()->request("post", "/shrink");
-    } catch (AccountException $err) {
-        if ($err->status == 429) return true;
-        throw $err;
-    } catch (ClientException $err) {
-        return true;
     }
 }
