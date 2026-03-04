bc4b8156d4de4519153f92ca6131818eae2e38e9
Tijmen <tijmenbruggeman@gmail.com>
2026-03-04 13:28:16 +0100

    feat(docs): add phpdocs
    * docs: add docs for methods and vars in Tinify class
    
    * Document client
    
    * Document Source
    
    * docs: result and resultmeta
    
    * Small improvements
    
    * Docs
    
    * Fix grammar in docs

--- lib/Tinify.php
+++ lib/Tinify.php
@@ -5,36 +5,89 @@ namespace Tinify;
 const VERSION = "1.6.4";
 
 class Tinify {
+    /**
+     * @var string|null API key.
+     */
     private static $key = NULL;
+
+    /**
+     * @var string|null Identifier used for requests.
+     */
     private static $appIdentifier = NULL;
+
+    /**
+     * @var string|null URL to the compression API.
+     */
     private static $proxy = NULL;
 
+    /**
+     * @var int|null The number of compressions.
+     */
     private static $compressionCount = NULL;
+
+    /**
+     * @var Client|null Tinify client.
+     */
     private static $client = NULL;
 
+    /**
+     * Sets the key and resets the client.
+     *
+     * @param string $key The API key.
+     * @return void
+     */
     public static function setKey($key) {
         self::$key = $key;
         self::$client = NULL;
     }
 
+    /**
+     * Sets the app identifier and resets the client.
+     *
+     * @param string $appIdentifier The app identifier.
+     * @return void
+     */
     public static function setAppIdentifier($appIdentifier) {
         self::$appIdentifier = $appIdentifier;
         self::$client = NULL;
     }
 
+    /**
+     * Sets the proxy and resets the client.
+     *
+     * @param string $proxy URL to the proxy server.
+     * @return void
+     */
     public static function setProxy($proxy) {
         self::$proxy = $proxy;
         self::$client = NULL;
     }
 
+    /**
+     * Retrieves the compression count.
+     *
+     * @return int|null
+     */
     public static function getCompressionCount() {
         return self::$compressionCount;
     }
 
+    /**
+     * Sets the compression count
+     *
+     * @param int $compressionCount
+     * @return void
+     */
     public static function setCompressionCount($compressionCount) {
         self::$compressionCount = $compressionCount;
     }
 
+    /**
+     * Retrieves the tinify client.
+     * Will initiate a new client with the current key, identifier and proxy.
+     *
+     * @return Client
+     */
     public static function getClient() {
         if (!self::$key) {
             throw new AccountException("Provide an API key with Tinify\setKey(...)");
@@ -47,6 +100,12 @@ class Tinify {
         return self::$client;
     }
 
+    /**
+     * Sets a new client
+     *
+     * @param Client $client
+     * @return void
+     */
     public static function setClient($client) {
         self::$client = $client;
     }
--- lib/Tinify/ResultMeta.php
+++ lib/Tinify/ResultMeta.php
@@ -2,25 +2,55 @@
 
 namespace Tinify;
 
+/**
+ * Class containing compressed image meta data
+ */
 class ResultMeta {
+    /**
+     * The response headers containing image metadata.
+     *
+     * @var array $meta
+     */
     protected $meta;
 
+    /**
+     * Constructs a ResultMeta with response metadata.
+     *
+     * @param array $meta The response headers containing image metadata.
+     */
     public function __construct($meta) {
         $this->meta = $meta;
     }
 
+    /**
+     * Width of the image in pixels.
+     * @return int The image width.
+     */
     public function width() {
         return intval($this->meta["image-width"]);
     }
 
+    /**
+     * Height of the image in pixels.
+     * @return int The image height.
+     */
     public function height() {
         return intval($this->meta["image-height"]);
     }
 
+    /**
+     * Location of the compressed image.
+     * @return string|null The URL to the compressed image, or null if not available.
+     */
     public function location() {
         return isset($this->meta["location"]) ? $this->meta["location"] : null;
     }
 
+    /**
+     * Retrieves the file extension of the image.
+     * Extracts the extension from the content-type header.
+     * @return string|null The file extension or null if not available.
+     */
     public function extension() {
         if (isset($this->meta["content-type"])) {
             $parts = explode("/", $this->meta["content-type"]);
