9c4c2a833c648c286e3a2e211f37bcf76cff8d6e
tijmen <tijmenbruggeman@gmail.com>
2026-03-18 17:10:27 +0100

    curl_init will never return null

--- lib/Tinify/Client.php
+++ lib/Tinify/Client.php
@@ -87,7 +87,7 @@ class Client {
             }
 
             $request = curl_init();
-            if ($request === false || $request === null) {
+            if ($request === false) {
                 throw new ConnectionException(
                     "Error while connecting: curl extension is not functional or disabled."
                 );
